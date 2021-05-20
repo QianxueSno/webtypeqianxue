3439c41e0e4c17bd778bd2d58946cda4008c7375
QianxueSno <qzhao01@risd.edu>
2021-05-20 19:07:55 -0400

    1

--- java.js
+++ java.js
@@ -41,3 +41,19 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+function setColor(e, btn, color) {
+  var target = e.target,
+      count = +target.dataset.count;
+
+   target.style.backgroundColor = count === 1 ? "#8582a2" : "black";
+   target.dataset.count = count === 1 ? 0 : 1;
+}
+
+function setse(e, btn, color) {
+  var target = e.target,
+      count = +target.dataset.count;
+
+   target.style.backgroundColor = count === 1 ? "#7FFF00" : "black";
+   target.dataset.count = count === 1 ? 0 : 1;
+}
